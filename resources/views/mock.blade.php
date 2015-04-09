5d1f77b6bd11a8fc8796ac7298f9b3a22ce0a44d
burgosa <alvaro.burgos@outlook.com>
2015-04-08 22:31:49 -0500

    dale duro

--- resources/views/mock.blade.php
+++ resources/views/mock.blade.php
@@ -13,10 +13,10 @@
     <title>Shop</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-    <link href="css/main.css" rel="stylesheet">
-    <link href="css/navbar.css" rel="stylesheet">
-    <link href="css/catalog.css" rel="stylesheet">
-    <link href="css/cart.css" rel="stylesheet">
+    <link href="/css/main.css" rel="stylesheet">
+    <link href="/css/navbar.css" rel="stylesheet">
+    <link href="/css/catalog.css" rel="stylesheet">
+    <link href="/css/cart.css" rel="stylesheet">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link href="http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,400italic" rel="stylesheet" type="text/css">
 
